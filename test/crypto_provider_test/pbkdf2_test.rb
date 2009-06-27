429f34aa7b5a80f8a68c25f445346d9dfcf239fc
anime4christ <anime4christ@gmail.com>
2009-06-27 16:32:18 -0700

    Updated Pbkdf2 test.

--- test/crypto_provider_test/pbkdf2_test.rb
+++ test/crypto_provider_test/pbkdf2_test.rb
@@ -3,12 +3,12 @@ require File.dirname(__FILE__) + '/../test_helper.rb'
 module CryptoProviderTest
   class Pbkdf2Test < ActiveSupport::TestCase
     def test_encrypt
-      assert Authlogic::CryptoProviders::Pbkdf2.encrypt("mypass")
+      assert Authlogic::CryptoProviders::Pbkdf2::Provider.encrypt("mypass")
     end
     
     def test_matches
-      hash = Authlogic::CryptoProviders::Pbkdf2.encrypt("mypass")
-      assert Authlogic::CryptoProviders::Pbkdf2.matches?(hash, "mypass")
+      hash = Authlogic::CryptoProviders::Pbkdf2::Provider.encrypt("mypass")
+      assert Authlogic::CryptoProviders::Pbkdf2::Provider.matches?(hash, "mypass")
     end
   end
 end
